ca8a6f69de99640598dd1eb7abfe0d66c07417ff
kraighamrick <kraighamrick@gmail.com>
2023-03-14 15:53:51 -0700

    vangogh2

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const painting = document.querySelector('.painting');
-painting.addEventListener('click', function(e){
-    e.painting.style.backroundColor = 'blue'
+let color = 'white';
 
+let colors = ['blue', 'red', 'yellow', 'green']; 
+for (let i = 0; i < colors.length; i++) { 
+     const chooser = document.querySelector('#' + colors[i]);
+     chooser.addEventListener('click', () => {
+         color = colors[i];
+    });
+}
+painting.addEventListener('click', (e) => {
+    e.target.style.backgroundColor = color
 })
